d8d4e5da845241b4e1175d47fd33ae7fcaf67e9c
abol313 <FazlAbol18@gmail.com>
2022-04-25 01:00:29 +0430

    enable writing time on label and padding zeros

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const shower = document.querySelector(".clock .shower")
+const letter = document.querySelector(".clock .letter .content")
 
 const ledWidth = 5, ledHeight = ledWidth
 
@@ -9,6 +10,7 @@ let cY = parseInt((rowNo - 1) / 2)
 cX = Math.max(0, Math.min(colNo - 1, cX))
 cY = Math.max(0, Math.min(rowNo - 1, cY))
 
+const radius = colNo/2
 
 let showerContent = ""
 for (let row = 0; row < rowNo; row++) {
@@ -37,8 +39,8 @@ function drawLine(x1, y1, x2, y2, strict = .5) {
             toggled &&= minX <= col && col <= maxX
             toggled &&= minY <= row && row <= maxY
             toggled &&= isPointOnLine(a, b, col, row, x1, y1, x2, y2, strict)
-            //if (toggled)
-            toggleLed(toggled, col, row)
+            if (toggled)
+                toggleLed(toggled, col, row)
         }
 }
 
@@ -51,12 +53,12 @@ function clear() {
 }
 
 function drawClockBase() {
-    for (let row = 0; row < rowNo; row++){
-        for (let col = 0; col < colNo; col++){
+    for (let row = 0; row < rowNo; row++) {
+        for (let col = 0; col < colNo; col++) {
             let toggeled = false;
-            toggeled ||= Math.abs(Math.sqrt((col-cX)**2 + (row-cY)**2)-(colNo-1)/2)<=.5
-            if(toggeled)
-                toggleLed(1,col,row)
+            toggeled ||= Math.abs(Math.sqrt((col - cX) ** 2 + (row - cY) ** 2) - (colNo - 1) / 2) <= .5
+            if (toggeled)
+                toggleLed(1, col, row)
         }
     }
 }
@@ -100,12 +102,22 @@ function toggleLed(mode, x, y) {
 //s : second
 function showTime(h, m, s) {
     clear()
+    showHand(h%12,12,radius/7*4)
+    showHand(m,60,radius/3*2)
+    showHand(s,60,radius-3.5)
 }
 
+//write time on letter
+//h : hour
+//m : minute
+//s : second
+function writeTime(h, m, s) {
+    letter.innerHTML = `${(h+"").padStart(2,"0")} : ${(m+"").padStart(2,"0")} : ${(s+"").padStart(2,"0")}`
+}
 function showHand(val, max, width) {
     val = val % max
     //center point of clock
-    let hX = parseInt(cX + Math.cos(val / (max / 2) * Math.PI) * width), hY = parseInt(cY + Math.sin(val / (max / 2) * Math.PI) * width)
+    let hX = parseInt(cX + Math.cos(val / (max / 2) * Math.PI - Math.PI/2 ) * width), hY = parseInt(cY + Math.sin(val / (max / 2) * Math.PI - Math.PI/2) * width)
 
     hY = Math.max(0, Math.min(rowNo - 1, hY))
     hX = Math.max(0, Math.min(colNo - 1, hX))
@@ -113,15 +125,21 @@ function showHand(val, max, width) {
     //console.log(cX, cY, hX, hY)
     drawLine(cX, cY, hX, hY)
 }
+
+
 // to test
 let inter
 function st() {
     let deg = 0
     inter = setInterval(() => {
         clear()
-        showHand(deg, 360, Math.ceil(colNo / 2))
-        deg += 10
-    }, 100)
+        let date = new Date();
+        let h = date.getHours()
+        let m = date.getMinutes()
+        let s = date.getSeconds()
+        showTime(h,m,s)
+        writeTime(h,m,s)
+    }, 1000)
 }
 function en() {
     clearInterval(inter)
