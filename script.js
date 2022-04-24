92c7aaeb24845c4ff521d548b7277c2b46a22ec7
abol313 <FazlAbol18@gmail.com>
2022-04-24 21:07:57 +0430

    reuired functions to draw on leds

--- script.js
+++ script.js
@@ -13,3 +13,38 @@ for(let row=0; row < rowNo ; row++){
 }
 
 shower.innerHTML = showerContent
+
+function drawLine(x1,y1,x2,y2){
+    //ax+b -> linear graph formula
+    let a = (y2-y1) / (x2-x1)
+    let b = y1 - a*x1
+    for(let row=0; row < rowNo ; row++)
+        for(let col=0 ; col < colNo ; col++)
+            if(isPointOnLine(a,b,row,col))
+                toggleLed(true,col,row)
+}
+
+//toggles off all leds
+function clear(){
+    for(let row=0; row < rowNo ; row++)
+        for(let col=0 ; col < colNo ; col++)
+            toggleLed(false,col,row)
+}
+
+
+//returns related boolean value (true / false)
+//to that is the point on the line(base of its formula ax+b)
+//a : ax+b
+//b : ax+b
+//x : point x
+//y : point y
+function isPointOnLine(a,b,x,y){
+    return a*x+b == y
+}
+
+//mode : true(turn on) / false(turn off)
+//x : led x position
+//y : led y position
+function toggleLed(mode,x,y){
+    document.querySelector(`.i${y}j${x}`).style.opacity = mode ? "1" : ".2"
+}
